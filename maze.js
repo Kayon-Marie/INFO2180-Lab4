ba0399c86ffbaf003463ca66d5dce517bc65dbcc
Kayon-Marie <k.marie.douglas@hotmail.com>
2018-10-19 15:42:48 -0500

    Alerts on successful completion of maze

--- maze.js
+++ maze.js
@@ -1,17 +1,28 @@
 window.onload = function(){
     let wall = 0;
     let boundaries = document.querySelectorAll(".boundary");
-    turnWalls(boundaries);
-}
+    turnWalls();
+    let end = document.getElementById("end");
+    endOfGame();
 
-function turnWalls(boundaries){
-    boundaries.forEach(b => {
-        b.addEventListener("mouseover", red);
-    });
-}
+    function turnWalls(){
+        boundaries.forEach(b => {
+            b.addEventListener("mouseover", red);
+        });
+    }
 
-function red(){
-    this.classList.add("youlose");
-    wall++;
-}
+    function red(){
+        this.classList.add("youlose");
+        wall++;
+    }
 
+    function endOfGame(){
+        end.addEventListener("mouseover", status);
+    }
+
+    function status(){
+        if(wall == 0){
+            window.alert("You win! Play again?");
+        }
+    }
+}
