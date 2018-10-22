1434e036c83b681f254d358f640a632793d1561b
Kayon-Marie <k.marie.douglas@hotmail.com>
2018-10-22 11:42:16 -0500

    Update of 'On-Page Status' and Completion of Disallow cheating

--- maze.js
+++ maze.js
@@ -1,17 +1,22 @@
 window.onload = function(){
     let wall = 0;
+    let endGame = false;
     let boundaries = document.querySelectorAll(".boundary");
-    turnWalls();
     let end = document.getElementById("end");
-    endOfGame();
     let start = document.getElementById("start");
-    startOfGame();
     let message = document.getElementById("status");
+    let maze = document.getElementById("maze");
+
     let original = message.innerHTML;
+
+    turnWalls();
+    startOfGame();
+    endOfGame();
+    cheat();
 
     function turnWalls(){
         boundaries.forEach(b => {
-            b.addEventListener("mouseover", red);
+            b.addEventListener('mouseover', red);
         });
     }
 
@@ -20,8 +25,19 @@ window.onload = function(){
         wall++;
     }
 
+    function makeAllRed() {
+        if(endGame===false){
+            boundaries.forEach(b => {
+                b.classList.add("youlose");
+                wall++;
+            });
+            status();
+        }
+    }
+
     function reset(){
         wall = 0;
+        endGame = false;
         message.innerHTML = original;
         boundaries.forEach(b => {
             b.classList.remove("youlose");
@@ -29,7 +45,12 @@ window.onload = function(){
     }
 
     function endOfGame(){
-        end.addEventListener("mouseover", status);
+        end.addEventListener("mouseover", gameEnd);
+    }
+
+    function gameEnd(){
+        endGame = true;
+        status();
     }
 
     function startOfGame(){
@@ -37,11 +58,22 @@ window.onload = function(){
     }
 
     function status(){
-        if(wall == 0){
-            message.innerHTML = "You win! Play again?";
-        }
-        else{
+        if(wall > 0 && endGame){
             message.innerHTML = "You lose! Try again?";
         }
+        else{
+            if(wall === 0 && endGame){
+                message.innerHTML = "You win! Play again?";
+            }
+            else{
+                if(wall > 0 && endGame === false){
+                    message.innerHTML = "You cheated!";
+                }
+            }
+        }
+    }
+
+    function cheat(){
+        maze.addEventListener("mouseleave", makeAllRed);
     }
 }
